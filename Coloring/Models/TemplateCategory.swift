fc045d6de38afc8c250ecc06ade885e868778f39
Don Noel <donnoel@icloud.com>
2026-03-15 16:26:44 -0700

    Add portrait drawing Jacks and map to Animals

--- Coloring/Models/TemplateCategory.swift
+++ Coloring/Models/TemplateCategory.swift
@@ -77,6 +77,7 @@ struct TemplateCategory: Codable, Identifiable, Hashable, Sendable {
             "winning"
         ],
         "Animals & Wildlife": [
+            "jacks",
             "cat and flag",
             "cats",
             "dogs",
