0934ea18980adb12122217cb78219d8895c0ead6
Don Noel <donnoel@icloud.com>
2026-04-06 06:54:26 -0700

    commit

--- scripts/run-on-jacks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-open -a Xcode /Users/donnoel/Development/Coloring/Coloring.xcodeproj
-osascript <<'APPLESCRIPT'
-tell application "Xcode" to activate
-delay 1
-tell application "System Events"
-    keystroke "r" using command down
-end tell
-APPLESCRIPT
